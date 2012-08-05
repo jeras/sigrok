704494cc9e7ec23694b70fe1ff2efaa4bcc2589a
Iztok Jeras <iztok.jeras@gmail.com>
2012-08-05 17:06:53 +0200

    onewire_link: documentation update

--- libsigrokdecode/decoders/onewire_link/__init__.py
+++ libsigrokdecode/decoders/onewire_link/__init__.py
@@ -42,48 +42,53 @@ overdrive communication speed. The following minimal values should be used:
 Probes:
 1-Wire requires a single signal, but some master implementations might have a
 separate signal used to deliver power to the bus during temperature conversion
-as an example. This power signal is currently not used.
+as an example. Changes to the power signal state are propagated to the next
+protocol layer.
 
  - owr (1-Wire signal line)
  - pwr (optional, dedicated power supply pin)
 
 Options:
+Only one option is documented, it is possible to configure the decoder to
+support overdrive mode or not (to avoid minimum samplerate warnings).
+
+ - overdrive (selected '1' by defaullt)
+
 1-Wire is an asynchronous protocol, so the decoder must know the samplerate.
 The timing for sampling bits, presence, and reset is calculated by the decoder,
 but in case the user wishes to use different values, it is possible to
-configure the following timing values (number of samplerate periods):
-
- - overdrive              (if active the decoder will be prepared for overdrive)
- - cnt_normal_bit         (time for normal mode sample bit)
- - cnt_normal_slot        (time for normal mode data slot)
- - cnt_normal_presence    (time for normal mode sample presence)
- - cnt_normal_reset       (time for normal mode reset)
- - cnt_overdrive_bit      (time for overdrive mode sample bit)
- - cnt_overdrive_slot     (time for overdrive mode data slot)
- - cnt_overdrive_presence (time for overdrive mode sample presence)
- - cnt_overdrive_reset    (time for overdrive mode reset)
-
-These options should be configured only on very rare cases and the user should
-read the decoder source code to understand them correctly.
+configure them. This options are not documented, since they should be used only
+if the master or slave requires timings slightly out of the standard, or if
+extremely low sampling rates are used. So the user should read the decoder
+source code if he/she wishes to use this options.
 
 Protocol output format:
-TODO.
+Protocol outputs are pairs [event, value]. The next event strings are
+available, value is limited to 0 or 1:
+
+ - 'RESET/PRESENCE' (the value is 1 if a presence pulse was received)
+ - 'BIT' (the value holds the bit being written or read)
+ - 'POWER' (power state changes to the current value are reported)
 
 Annotations:
+Two annotation options are available:
 
-Link layer annotations show the following events:
+ - 'Text' (provides protocol events as short human readable text)
+ - 'Timing' (protocol events are accompanied with timing details and warnings)
 
- - NOTE/WARNING/ERROR
-   Possible samplerate related timing issues are reported.
- - Reset/presence true/false
-   The event is marked from the signal negative edge to the end of the reset
-   high period. It's also reported if there are any devices attached to the bus.
- - Bit 0/1
+Link layer 'Text' annotations show the following events:
+
+ - RESET/PRESENCE: True/False
+   The event is marked from the signal falling edge to the end of the presence
+   pulse or the window in which such a pulse is expected. It's also reported
+   if there are any devices attached to the bus.
+ - BIT: 0/1
    The event is marked from the signal negative edge to the end of the data
    slot. The value of each received bit is also provided.
+ - POWER: Applied/Removed
+   Every change to the power state is reported.
 
 TODO:
-- Check for protocol correctness, if events are timed inside prescribed limits.
 - Maybe add support for interrupts, check if this feature is deprecated.
 '''
 
